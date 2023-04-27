f30c3a0baeef5a1cb0e49e93afb54962b3b08acd
Alexandr Antonov <antonovsacha@yandex.ru>
2023-04-27 20:32:10 +0300

    codeStyle: 1 попытка

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
@@ -8,14 +8,23 @@ import java.util.Optional;
 
 public interface ReviewStorage {
     Optional<Review> get(Integer id);
+
     Review add(Review review);
+
     Review update(Review review);
+
     Integer remove(Integer id);
+
     boolean containsKey(Integer id);
+
     boolean containsValue(Review review);
+
     Collection<Review> getAll();
+
     Collection<Review> getReviewsFromFilm(Integer filmId, Integer count);
+
     boolean addUserGrade(Integer reviewId, Integer userId, Grade grade);
+
     boolean deleteUserGrade(Integer reviewId, Integer userId, Grade grade);
 
 
